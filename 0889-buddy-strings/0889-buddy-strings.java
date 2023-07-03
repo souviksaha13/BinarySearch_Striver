5a6baf50a083aefbf9bb2872e05f2925557c1845
Souvik Saha <134754803+souviksaha13@users.noreply.github.com>
2023-07-03 10:14:02 +0530

    Time: 1 ms (99.01%), Space: 41.6 MB (57.51%) - LeetHub

---
+++ 0889-buddy-strings/0889-buddy-strings.java
@@ -0,0 +1,30 @@
+class Solution {
+    public boolean buddyStrings(String s, String goal) {
+        if(s.length() != goal.length()) return false;
+
+        if(s.equals(goal)) {
+            HashMap<Character, Integer> mp = new HashMap<>();
+            for(int i=0; i<s.length(); i++) {
+                if(mp.containsKey(s.charAt(i))) return true;
+                else mp.put(s.charAt(i), 1);
+            }
+            return false;
+        }
+
+        else {
+            int count = 0;
+            int[] dif = new int[2];
+
+            for(int i=0; i<s.length(); i++) {
+                if(s.charAt(i) != goal.charAt(i)) {
+                    if(++count > 2) return false;
+                    dif[count-1] = i;
+                }
+            }
+            if(count != 2) return false;
+            if(s.charAt(dif[0]) == goal.charAt(dif[1]) && s.charAt(dif[1]) == goal.charAt(dif[0])) return true;
+        }
+
+        return false;
+    }
+}
