05873ac43c1667791ba31b8aa18c20f80c5dffac
Souvik Saha <134754803+souviksaha13@users.noreply.github.com>
2023-09-03 23:55:19 +0530

    Time: 0 ms (100%), Space: 39.3 MB (44.1%) - LeetHub

---
+++ 0062-unique-paths/0062-unique-paths.java
@@ -0,0 +1,50 @@
+class Solution {
+    
+    public int uniquePaths(int m, int n) {
+/*    // Recursive solution
+    public int uniquePaths(int m, int n) {
+        if(m==1 && n==1) return 1;
+        if(m==0 || n==0) return 0;
+
+        return  (uniquePaths(m-1, n) + uniquePaths(m, n-1));
+    }
+*/
+
+/*    // Memoization Method
+    public int uniquePaths(int m, int n) {
+        
+        int[][] dp = new int[m][n];
+        for(int i=0; i<m; i++){
+            for(int j=0; j<n; j++){
+                dp[i][j] = -1;
+            }
+        }
+        return rec(m-1, n-1, dp);
+    }
+
+    public int rec(int m, int n, int[][] dp){
+        if(m==0 && n==0) return 1;
+        if(m==-1 || n==-1) return 0;
+
+        if(dp[m][n] != -1) return dp[m][n];
+        dp[m][n] = rec(m-1, n, dp) + rec(m, n-1, dp);
+        return dp[m][n];
+    }
+*/
+        // Tabulation Method
+        int[][] dp = new int[m][n];
+        for(int i=0; i<m; i++){
+            for(int j=0; j<n; j++){
+                dp[i][j] = -1;
+            }
+        }
+        
+        for(int i=0; i<m; i++){
+            for(int j=0; j<n; j++){
+                if(i==0 || j==0) dp[i][j] = 1;
+                else dp[i][j] = dp[i-1][j] + dp[i][j-1];
+            }
+        }
+        return dp[m-1][n-1];
+    }
+}
