f4868a2d5e14544356d52f5d5fc1db43267e9366
Souvik Saha <134754803+souviksaha13@users.noreply.github.com>
2023-09-18 18:26:53 +0530

    Time: 13 ms (5.4%), Space: 44.5 MB (75.35%) - LeetHub

--- 1463-the-k-weakest-rows-in-a-matrix/1463-the-k-weakest-rows-in-a-matrix.java
+++ 1463-the-k-weakest-rows-in-a-matrix/1463-the-k-weakest-rows-in-a-matrix.java
@@ -1,25 +1,23 @@
 class Solution {
     public int[] kWeakestRows(int[][] mat, int k) {
-        int n = mat.length, m = mat[0].length;
-        int[][] pair = new int[n][2];
+        int n = mat.length;
+        int m = mat[0].length;
         int[] ans = new int[k];
-
-        for(int i=0; i<n; i++) {
-            int cnt = 0;
-            for(int j=0; j<m; j++) {
-                if(mat[i][j] == 1) cnt++;
-                else break;
+        int cnt = 0;
+        Set<Integer> hs = new HashSet<>();
+        loop:
+        for(int j=0; j<=m; j++) {
+            for(int i=0; i<n; i++) {
+                if(j == m || mat[i][j] == 0) {
+                    if(!hs.contains(i)) {
+                        ans[cnt++] = i;
+                        System.out.println(i + " " + j);
+                        hs.add(i);
+                    }
+                    if(cnt == k) break loop;
+                }
             }
-            pair[i][0] = cnt;
-            pair[i][1] = i;
         }
-
-        Arrays.sort(pair, (a,b) -> a[0]-b[0]);
-
-        for(int i=0; i<k; i++) {
-            ans[i] = pair[i][1];
-        }
-
         return ans;
     }
 }
