ee39c9d596a4dd8ee36c336d811ea2648a70ddd5
Souvik Saha <134754803+souviksaha13@users.noreply.github.com>
2023-09-25 10:12:20 +0530

    Time: 2 ms (68.98%), Space: 40.7 MB (71.69%) - LeetHub

---
+++ 0389-find-the-difference/0389-find-the-difference.java
@@ -0,0 +1,15 @@
+class Solution {
+    public char findTheDifference(String s, String t) {
+        char[] sa = s.toCharArray();
+        char[] ta = t.toCharArray();
+        
+        Arrays.sort(sa); Arrays.sort(ta);
+        
+        for(int i=0; i<sa.length; i++){
+            if(sa[i] != ta[i]){
+            return ta[i];
+            }
+        }
+        return ta[sa.length];
+    }
+}
